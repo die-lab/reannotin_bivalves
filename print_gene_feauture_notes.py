4d18065a4fa0157120a2223308551a5167a93f46
die-lab <carlidiego97@outlook.it>
2024-03-20 19:42:16 +0100

    updates

---
+++ print_gene_feauture_notes.py
@@ -0,0 +1,21 @@
+from Bio import SeqIO
+from Bio.Seq import Seq
+import sys
+
+genbank_file = sys.argv[1]
+genes_name_list = { 'ATP6':['ATP6', 'ATPASE 6'], 'ATP8':['ATP8'], 'CYTB':['COB','CYTB'], 'COX1':['COI','COX1','COXI'], 'COX2':['COII','COX2','COXII'], 'COX3':['COIII','COX3','COXIII'], 'FORF':['F-ORF', 'FORF'], 'HORF':['HORF'], 'MORF':['M-ORF', 'MORF'],'NAD1':['NAD1','ND1'],'NAD2':['NAD2', 'ND2'],'NAD3':['NAD3','ND3'],'NAD4':['NAD4', 'ND4'],'NAD4L':['NAD4L', 'ND4L','NDL'],'NAD5':['NAD5', 'ND5'],'NAD6':['NAD6', 'ND6']}
+genes_name_list = { 'ATP6':['ATP6', 'ATPASE 6','ATP SYNTHASE F0 SUBUNIT 6','ATP SYNTHASE SUBUNIT 6','ATP SYNTHETASE SUBUNIT 6','ATPASE SUBUNIT 6'], 'ATP8':['ATP SYNTHASE F0 SUBUNIT 8','ATP8','TRUNCATED ATP SYNTHASE F0 SUBUNIT 8','ATP SYNTHASE SUBUNIT 8'], 'CYTB':['COB','CYTB','CYTOCHROME B'], 'COX1':[',CYTOCHROME C OXIDASE SUBUNIT 1','COI','COX1','COXI','CYTOCHROME C SUBUNIT I','CYTOCHROME C OXIDASE SUBUNIT I'], 'COX2':['CYTOCHROME C OXIDASE SUBUNIT 2','COII','COX2','COXII','CYTOCHROME C SUBUNIT II','CYTOCHROME OXIDASE C SUBUNIT 2','CYTOCHROME C OXIDASE SUBUNIT II'], 'COX3':['CYTOCHROME C OXIDASE SUBUNIT III','CYTOCHROME C OXIDASE SUBUNIT 3','COIII','COX3','COXIII','CYTOCHROME OXIDASE C SUBUNIT 3','CYTOCHROME C SUBUNIT III'], 'FORF':['F-ORF', 'FORF','F-SPECIFIC ORF PROTEIN','F ORF'], 'HORF':['HORF','H OPEN READING FRAME'], 'MORF':['M-SPECIFIC MORF PROTEIN','M-ORF', 'MORF','M-SPECIFIC ORF PROTEIN','M-ORF PROTEIN','M-ORF1','M-ORF2','MORF21'],'NAD1':['NAD1','ND1','NADH DEHYDROGENASE SUBUNIT 1'],'NAD2':['NAD2', 'ND2','NADH DEHYDROGENASE SUBUNIT 2'],'NAD3':['NAD3','ND3','NADH DEHYDROGENASE SUBUNIT 3'],'NAD4':['NAD4', 'ND4','NADH DEHYDROGENESE SUBUNIT 4','NADH DEHYDROGENASE SUBUNIT 4'],'NAD4L':['NADH DEHYDROGENASE SUBUNIT 4L','NADH DEHYDROGENASE SUBUNIT L','NAD4L', 'ND4L','NDL'],'NAD5':['NAD5', 'ND5','NADH DEHYDROGENASE SUBUNIT 5'],'NAD6':['NADH DEHYDROGENASE SUBUNIT 6','NAD6', 'ND6']}
+
+for record in SeqIO.parse(genbank_file, 'genbank'):
+	species_db = dict(zip(list(genes_name_list), [[] for i in list(genes_name_list)]))	
+	gene_complete_check = []	
+	for feature in record.features:
+		if feature.type == 'CDS':
+			gene_name = feature.qualifiers.get('gene', [''])[0].upper()
+			note = feature.qualifiers.get('note', [''])[0].upper()
+			if len(note) > 0:
+				print(record.id)
+				print(note)       	
+
+     	
+
---
+++ stop_codons_check.py
@@ -0,0 +1,74 @@
+import re
+import os
+import matplotlib.pyplot as plt
+import numpy as np
+from Bio import SeqIO
+from Bio.Seq import Seq
+
+#mitochondrial invertebrate start codons from ncbi
+alt_codons = ['ATG','ATA','ATT','ATC','GTG','TTG']
+
+mito_genes = ['ATP6', 'ATP8', 'CYTB', 'COX1', 'COX2', 'COX3', 'FORF', 'HORF', 'MORF', 'NAD1', 'NAD2', 'NAD3', 'NAD4', 'NAD4L', 'NAD5', 'NAD6']
+
+def codons_of_three(input):	
+	if (len(record.seq)/3 % 1) > 0:
+		print("warning: "+ input.id + " has an unusual length")
+		w = 1
+	else:
+		w = 0
+	return w, len(record.seq)/3	
+
+def plot_start(alternative_dict, gene, heigth, weird):
+	start_counts = {}
+	for codon in alt_codons: 
+		counts = np.fromiter(alternative_dict[gene][codon].values(), dtype=int)
+		positions = np.fromiter(alternative_dict[gene][codon].keys(), dtype=int)
+		start_counts[codon] = counts
+	fig, ax = plt.subplots()
+	bottom = np.zeros(len(positions))
+	for sex, start_count in start_counts.items():
+		p = ax.bar(positions, start_count, label=sex, bottom=bottom, width=0.5)
+		bottom += start_count
+		#ax.bar_label(p, label_type='center')
+	ax.set_title(str(gene + ' alternative start codons distribution'))
+	ax.legend(loc='upper right')
+	ax.axhline(heigth)
+	weird_to_height_ratio = weird / heigth
+	ax.axhline(heigth-weird, color='black', ls='--', linewidth=0.5)
+	plotname = str(gene+'_start_codons.svg')
+	#plt.savefig(plotname)
+
+current_directory = os.getcwd()
+files_in_directory = os.listdir(current_directory)
+
+alternative_dict = {}
+for gene in mito_genes:
+	lookup = str('_'+gene+'_')		
+	gene_files = [file for file in files_in_directory if lookup in file and file.endswith('.fasta')]
+	print(gene)
+	if len(gene_files) == 0:
+		continue
+	#how many of them have a weird length  
+	weird_length = 0	
+	length_tot = []
+	for file in gene_files:
+		record = SeqIO.read(file,"fasta")
+		cot = codons_of_three(record)
+		length_tot.append(cot[1])		
+		weird_length = weird_length + cot[0]
+	weird_length_fraction = weird_length/len(gene_files)
+	print(weird_length_fraction, max(length_tot))
+	alternative_dict[gene] = {}	
+	for alternative in alt_codons:	
+		my_dict = {i: 0 for i in range(1, int(max(length_tot))+1)}	
+		for file in gene_files:
+			record = SeqIO.read(file,"fasta")		
+			matches = re.finditer(alternative,str(record.seq))
+			positions = [match.end() for match in matches]
+			pos_corr = [(element / 3) for element in positions if (element % 3 == 0)]
+			for x in pos_corr:
+				my_dict[x] = my_dict[x] + 1
+		alternative_dict[gene][alternative] = my_dict
+	plot_start(alternative_dict,gene,len(gene_files),weird_length)
+
+
